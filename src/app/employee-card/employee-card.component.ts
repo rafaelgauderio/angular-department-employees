11071546f877537b6140101a2b1003a731d2de01
Rafael de Luca <deluca1712@gmail.com>
2025-05-16 17:33:45 -0300

    directive ngStyle

--- src/app/employee-card/employee-card.component.ts
+++ src/app/employee-card/employee-card.component.ts
@@ -5,12 +5,25 @@ import { Component, Input } from '@angular/core';
   selector: 'app-employee-card',
   standalone: false,
   templateUrl: './employee-card.component.html',
-  styleUrl: './employee-card.component.css'
+  //styleUrl: './employee-card.component.css'
+  styles: [`
+    .card-block {
+      text-transform: capitalize;
+      color: green;
+    }`]
 })
+
 export class EmployeeCardComponent {
-    @Input() employee : any = {
-      id: 0,
-      name: '',
-      department: ''
+  @Input() employee: any = {
+    id: 0,
+    name: '',
+    department: ''
+  };
+
+  getCardStyle() {
+    return {
+      backgroundColor: this.employee.id%2===0 ? 'lightyellow' : 'lightgreen',
+      borderColor: this.employee.id%2===0 ? 'yellow': 'green'
     };
+  }
 }
