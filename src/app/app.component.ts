20aad3326432f54ffa8b01a72992c1458fe49c31
Rafael de Luca <deluca1712@gmail.com>
2025-06-03 11:04:28 -0300

    cleaning project, change to angular 16 dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
-import { HelloComponent } from "./hello/hello.component";
-import { HeaderComponent } from "./header/header.component";
 //import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  //imports: [RouterOutlet],
-  standalone: false,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'frontend-funcionarios-departamentos';
-   emplooyees: Employee[] = [];
-activeIndex: any;
-
-   whenAdding (emp : Employee) {
-    this.emplooyees.push(emp);
-
-   }
+  title = 'frontend-funcionarios-departamentos';
 }
-interface Employee {
-  id: number,
-  name: string,
-  department: string
-}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HelloComponent } from './hello/hello.component';
 import { FormsModule, NgForm } from '@angular/forms';
-import { EmployeeCardComponent } from './employee-card/employee-card.component';
-import { EmployeeFormComponent } from './employee-form/employee-form.component';
+
 import { TabViewModule } from 'primeng/tabview';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { BadgeModule } from 'primeng/badge';
-import { OverlayBadgeModule } from 'primeng/overlaybadge';
+//import { OverlayBadgeModule } from 'primeng/overlaybadge';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, HelloComponent, EmployeeCardComponent, EmployeeFormComponent
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, TabViewModule, AvatarModule, AvatarGroupModule,
-    BadgeModule, OverlayBadgeModule, ScrollPanelModule, ButtonModule
-
-
+    FormsModule, TabViewModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
