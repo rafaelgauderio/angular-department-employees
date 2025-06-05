7ca6b5fedb32c755b9d21fdcdaa622515a3a4ef3
Rafael de Luca <deluca1712@gmail.com>
2025-06-05 17:26:49 -0300

    ngForm and ngSubmit

--- src/app/departamento-cadastro/departamento-cadastro.component.ts
+++ src/app/departamento-cadastro/departamento-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-departamento-cadastro',
@@ -13,6 +14,11 @@ export class DepartamentoCadastroComponent {
   exclusionDate = new Date(2025, 1, 20);
   value = 122034.5030
   value02 = 81712.8025
+
+  salvar (departmentForm :NgForm) {
+    console.log("Nome do departamento: " + departmentForm.value.nome);
+    console.log("Empresa do departamento: " + departmentForm.value.empresa);
+  }
 
   toCamelCase(str: string) {
     return str.replace(/(\s|-|_|\.)(\w)/g, function (match, sep, letter) {
