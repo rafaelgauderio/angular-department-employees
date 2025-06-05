47f5867cf94145ee12d2f4296c7f11e652bf26ee
Rafael de Luca <deluca1712@gmail.com>
2025-06-05 15:33:03 -0300

    pipes

--- src/app/departamento-cadastro/departamento-cadastro.component.ts
+++ src/app/departamento-cadastro/departamento-cadastro.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class DepartamentoCadastroComponent {
 
+  name = "Comercial Interno";
+  registrationDate = new Date(2024, 1, 15);
+  updateDate = new Date(2024, 4, 29);
+  exclusionDate = new Date(2025, 1, 20);
+  value = 122034.5030
+  value02 = 81712.8025
+
+  toCamelCase(str: string) {
+    return str.replace(/(\s|-|_|\.)(\w)/g, function (match, sep, letter) {
+      return letter.toUpperCase();
+    }).replace(/(^\w)/, function (letter) {
+      return letter.toLowerCase();
+    });
+  }
+
 }
+
